d26c3df74b316d218c599b1fbdabb5bc56e008d2
michael <1013772062@qq.com>
2024-10-25 20:42:13 +0800

    增加db3转csv

--- bag2csv/bag2csv.py
+++ bag2csv/bag2csv.py
@@ -16,13 +16,15 @@ with rosbag.Bag(bag_file, 'r') as bag:
     for topic, msg, t in bag.read_messages(topics=[topic_name]):
         # 提取时间戳
         timestamp = msg.header.stamp.to_sec()  # 将时间戳转换为秒
-        
+
         # 获取字段名称
         if frame_id == 0:
-            columns = ['frame_id', 'timestamp'] + [field.name for field in msg.fields]
-        
+            columns = ['frame_id', 'timestamp'] + \
+                [field.name for field in msg.fields]
+
         # 提取点云数据
-        pc_gen = pc2.read_points(msg, field_names=columns[2:], skip_nans=True)  # 从第三列开始提取点云数据
+        pc_gen = pc2.read_points(
+            msg, field_names=columns[2:], skip_nans=True)  # 从第三列开始提取点云数据
         for point in pc_gen:
             # 将帧序号、时间戳和点云信息组合在一起
             all_data.append((frame_id, timestamp) + point)
@@ -34,5 +36,5 @@ df = pd.DataFrame(all_data, columns=columns)
 
 # 保存为 CSV 文件
 df.to_csv(output_csv, index=False)
-print(f"PointCloud2 data with frame ID and timestamp has been saved to {output_csv}")
-
+print(
+    f"PointCloud2 data with frame ID and timestamp has been saved to {output_csv}")
